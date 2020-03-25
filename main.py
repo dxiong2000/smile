a62ea8d467e6586d932b174b1dd5a04e7a2288d0
Daniel Xiong <dxiong2000@gmail.com>
2020-03-24 20:46:15 -0700

    bug fixes

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ import os
 
 REFERENCE_PATH = "D:\Projects\smile\images\\reference"
 
+
 def compare(image, reference):
-    '''
-    :param image:
-    :param reference:
+    """
+    :param image: webcam image
+    :param reference: reference image
     :return: Boolean for whether the webcam image is a match with the reference image.
-    '''
+    """
 
     # gets encoding for reference face
     reference_face_encoding = fr.face_encodings(reference)[0]
     # gets list of possible face encodings for unknown image
     image_face_encodings = fr.face_encodings(image)
+
+    # draws rectangle around face
+    # ref_face_location = fr.face_locations(reference)[0]
+    # (top, right, bottom, left) = ref_face_location
+    # # Draw a box around the face
+    # cv2.rectangle(reference, (left, top), (right, bottom), (0, 0, 255), 2)
+    # cv2.imshow('reference face', reference)
 
     # iterates through possible face encodings, and compares to reference.
     for face_encoding in image_face_encodings:
@@ -30,25 +38,28 @@ def compare(image, reference):
 
 
 def login():
-    '''
+    """
     Logs into Windows
     :return:
-    '''
+    """
 
     print('logged in!')
     return
 
 
 def take_reference_image():
-    '''
+    """
     :return: Reference image
-    '''
+    """
     webcam = cv2.VideoCapture(0)
     ret, image = webcam.read()
+    while not ret:
+        ret, image = webcam.read()
     image = cv2.flip(image, 1)
     cv2.imwrite('D:\Projects\smile\images\\reference\{}.jpg'.format(datetime.now().strftime("%H%M%S")), image)
     webcam.release()
     return image
+
 
 def main():
     reference = None
@@ -71,14 +82,14 @@ def main():
             use_this_frame = True
             continue
 
+        # gets frame from webcam
         ret, image = webcam.read()
+        while not ret:
+            ret, image = webcam.read()
 
-        if not ret:
-            print("Webcam failed.")
-            sys.exit(0)
-
+        # compares reference image with new image
         if compare(image, reference):
-            # closes webcam
+            # closes webcam and goes to login function
             webcam.release()
             login()
             return
